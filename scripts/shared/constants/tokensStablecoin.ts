62ad8c1eaab8c42f26732f0cac4913cf67715bb0
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-09-17 15:10:44 -0400

    add more polygon stablecoin

--- scripts/shared/constants/tokensStablecoin.ts
+++ scripts/shared/constants/tokensStablecoin.ts
@@ -28,6 +28,16 @@ const tokensStablecoin: ITokenStablecoin[] = [
     },
     {
       network: 'polygon',
+      address: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
+      symbol: 'DAI'
+    },
+    {
+      network: 'polygon',
+      address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
+      symbol: 'USDT'
+    },
+    {
+      network: 'polygon',
       address: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
       symbol: 'WMATIC'
     },
